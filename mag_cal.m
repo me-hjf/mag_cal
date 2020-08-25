606ed338d132cbb4dfbb541e455868db11d7367c
David Goncalves <davegoncalves@gmail.com>
2020-08-25 07:54:46 -0400

    Initial commit

---
+++ mag_cal.m
@@ -0,0 +1,70 @@
+function [A_1, b, t, std_err] = mag_cal(s, F=1)
+    # get an ellipsoid fit to data
+    [M, n, d] = ellipsoid_fit(s);
+    # calibration parameters A_1 and b
+    M_1 = inv(M);
+    b = (-M_1 * n)';
+    A_1 = real(F / sqrt(n' * (M_1 * n) - d) * sqrtm(M));
+    # calibrated data  and std. err.
+    t = (s - b) * A_1;
+    r_diff = norm(t, 2, 'rows') - 1;
+    std_err = sqrt(var(r_diff)) / sqrt(rows(r_diff));
+endfunction
+
+function [M, n, d] = ellipsoid_fit (s)
+    # Refer to https://teslabs.com/articles/magnetometer-calibration/
+    # for derivation of magnetic calibration through an ellipsoidal fit 
+    
+    # D (samples)
+    a1 = s(:,1) .^ 2; #a
+    a2 = s(:,2) .^ 2; #b
+    a3 = s(:,3) .^ 2; #c
+    a4 = 2 * s(:,2) .* s(:,3); #f
+    a5 = 2 * s(:,1) .* s(:,3); #g
+    a6 = 2 * s(:,1) .* s(:,2); #h
+    a7 = 2 * s(:,1); #p
+    a8 = 2 * s(:,2); #q
+    a9 = 2 * s(:,3); #r
+    a10 = ones(rows(s(:,1)),1); #d
+    D = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10];
+
+    # S, S_11, S_12, S_21, S_22 (eq.11)
+    S = D' * D;
+    S_11 = S(1:6,1:6);
+    S_12 = S(1:6,7:end);
+    S_21 = S(7:end,1:6);
+    S_22 = S(7:end,7:end);
+
+    # C (Eq. 8, k=4)
+    C = [ -1, 1, 1, 0, 0, 0;
+          1, -1, 1, 0, 0, 0;
+          1, 1, -1, 0, 0, 0
+          0, 0, 0, -4, 0, 0;
+          0, 0, 0, 0, -4, 0;
+          0, 0, 0, 0, 0, -4;];
+
+    # v_1 (eq. 15, solution)
+    E = inv(C) * (S_11 - (S_12 * (inv(S_22) * S_21)));
+    [v, l, w] = eig(E);
+    E_w = diag(l);
+    E_v = v;
+    [m, i] = max(E_w);
+    v_1 = v_1 = E_v(:,i);
+    if v_1(1) < 0
+      v_1 = -v_1;
+    endif;
+
+    # v_2 (eq. 13, solution)
+    
+    v_2 = (v_1' * (-inv(S_22) * S_21)');
+      
+    # quadric-form parameters
+    # M = [a, h, g; h, b, f; g, f, c]
+    M = [v_1(1), v_1(4), v_1(5); 
+         v_1(4), v_1(2), v_1(6); 
+         v_1(5), v_1(6), v_1(3)];
+    # n = [p; q; r]
+    n = [v_2(1); v_2(2); v_2(3)];
+    d = v_2(4);
+
+endfunction
---
+++ plot_cal.m
@@ -0,0 +1,35 @@
+function [A_1, b, t, std_err] = plot_cal(s)
+# Generates magnetic calibration parameters, 
+# a calibrated data set, and pre-/post- plots
+
+#Set limits and major labels
+figure;
+xlim([-1,1]);
+ylim([-1,1]);
+zlim([-1,1]);
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+hold all;
+
+#plot unit sphere and set perspective
+[x y z] = sphere;
+usph=surf(x, y, z);
+set(usph,'facealpha',0.5);
+set(usph,'handlevisibility','off');
+axis equal;
+daspect([1 1 1])
+view(30,10)
+
+#plot uncalibrated data
+scatter3(s(:,1), s(:,2), s(:,3), [], 'r', 'filled')
+
+#get calibrated data
+[A_1, b, t, std_err] = mag_cal(s);
+
+#plot calibrated data
+scatter3(t(:,1), t(:,2), t(:,3), [], 'b','filled')
+legend('Raw Data', 'Calibrated')
+
+#pretty title with standard error
+title(cstrcat("Magnetic Ellipsoidal Fit Calibration\nStd. Err: ", num2str(std_err)));
